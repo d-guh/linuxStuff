a02ee1c59bd0c0f3cc6296ee1ca5c45ba151621a
D-Guy <ddcool535@gmail.com>
2025-08-16 11:22:37 -0400

    Added note to script

--- dguy/testsh.sh
+++ dguy/testsh.sh
@@ -10,3 +10,8 @@ echo "\$version:               $version"
 echo "\$BASH:                  $BASH"
 echo "\$shell:                 $shell"
 echo "\$ZSH_NAME:              $ZSH_NAME"
+
+# I'd prefer to use the shell specific ones if able, but tools like su when doing a login shell can be quite picky
+# For example, if you're trying to 'sudo su root -l -s /bin/bash' but roots login shell in /etc/passwd is: '/usr/sbin/nologin'
+# It will display: [nologin] root@dguyserv:~# , even though the current shell IS BASH
+# This kinda sucks and I'm about to either use ps or hardcode it tbh
