15690fc5800d823ca974b85e83d69c6e57933e63
D-Guy <ddcool535@gmail.com>
2025-08-24 16:46:56 -0400

    Updated startup script, long keyid format for gpg

--- opt/automation/scripts/startup-alert-wifi.sh
+++ opt/automation/scripts/startup-alert-wifi.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
 # startup-alert.sh
-# Sends a message when the server starts and connects
+# Performs some actions and sends a message when the server starts and connects
 # crontab: @reboot /opt/automation/scripts/startup-alert.sh
 #
 # Dependencies:
 # - curl
 # - logger
 # - date
+# - sudo
+# - iwconfig (from wireless-tools)
 
 # Config Vars
 WEBHOOK_URL="" # ADD URL HERE
 LOG_TAG="startup-alert"
 
-sleep 10 # Allow time for inet
+sleep 1
+# Disable power saving mode for the wifi card
+sudo iwconfig wlp12s0 power off
+logger -t startup-alert "Server started, power saving disabled."
+
+# Allow time for WiFi to connect
+sleep 10
 
 SERVER_TIME=$(date +"%Y-%m-%d %H:%M:%S %Z")
 MESSAGE="<@348121438169071618> :green_circle: :white_check_mark:\\n**Server has started successfully**\\nServer Time: \`$SERVER_TIME\`"
